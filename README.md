ee542349285f23dcd6b4a7813bcf07fac498a617
jelizasilatei <silateijc90@gmail.com>
2023-03-21 12:22:32 +0300

    Speed Detector README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Code_Challenge-week1
 
-##Student Grade Generator
+## Student Grade Generator
 
 This code generates a simple web page that prompts the user to input a student's marks and calculates and displays the corresponding grade based on the input. The grades are calculated as follows:
 
@@ -10,17 +10,31 @@ This code generates a simple web page that prompts the user to input a student's
 * D: 40 to 49
 * E: less than 40
 
-#How to use
+## Speed Detector
+This program is designed to take as input the speed of a car and determine the number of demerit points the driver should receive, based on the speed limit and the amount by which the speed exceeds the limit.
+
+# How to use
 1. Clone or download the repository.
 2. Open the marks.html file in your web browser.
-3. Enter the student's marks in the input field.
-4. The corresponding grade will be displayed in the output field.
+3. The program will automatically load and be ready for use.
 
-#Technologies used
+4. For Student grade generator: 
+* Enter the student's marks in the input field.
+* Click the calculate Grade button.
+* The corresponding grade will be displayed in the output field.
+
+5. For Speed detector:
+* Navigate to the speed generator in the nav bar.
+* Enter the speed of the car in km/s into the input field.
+* Click the check speed button.
+* The program will display either the number of demerit points or "License suspended", depending on whether the driver has accumulated more than 12 demerit points.
+
+
+# Technologies used
 This code was written in JavaScript and uses HTML and CSS to display the input and output fields.
 
-#License
+# License
 This code is licensed under the MIT License. You are free to use, modify, and distribute it as you like.
 
-#Credits
+# Credits
 This code was written by [Jeliza Silatei] and was based on a toy problem from [Insert Source Here].
--- speed.js
+++ speed.js
@@ -1,31 +1,38 @@
+// This code listens for the DOMContentLoaded event, then gets references to the relevant HTML elements.
 document.addEventListener('DOMContentLoaded', function() {
-  const speedInput = document.getElementById('speed-input');
-  //const submitBtn = document.getElementById('submit-btn');
-  const resultDiv = document.getElementById('result');
-  let form = document.getElementById("form");
+    const speedInput = document.getElementById('speed-input');
+    //const submitBtn = document.getElementById('submit-btn');
+    const resultDiv = document.getElementById('result');
+    let form = document.getElementById("form");
+    
+    //Attach an event listener to the form's submit event to call the calculateSpeed function.
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      calculateSpeed();
+ 
+    });
 
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    calculateSpeed();
-
+    // The calculateSpeed function reads the input value and calculates points based on the speed.
+    function calculateSpeed() {
+      const speed = parseInt(speedInput.value);
+      let points = 0;
+      
+      // The result is then displayed in the resultDiv element.
+      if (speed <= 70) {
+        // If the speed is less than or equal to 70, "OK" is displayed in the resultDiv.
+        resultDiv.textContent = 'OK';
+        return;
+      }
+      points = Math.floor((speed - 70) / 5);
+      
+      if (points >= 12) {
+        // If the calculated points are greater than or equal to 12, "License suspended" is displayed in the resultDiv.
+        resultDiv.textContent = 'License suspended';
+      } else {
+        // Otherwise, "Points: [points]" is displayed in the resultDiv.
+        resultDiv.textContent = 'Points: ' + points;
+      }
+    }
+    
   });
-
-  function calculateSpeed() {
-    const speed = parseInt(speedInput.value);
-    let points = 0;
-
-    if (speed <= 70) {
-      resultDiv.textContent = 'OK';
-      return;
-    }
-
-    points = Math.floor((speed - 70) / 5);
-
-    if (points >= 12) {
-      resultDiv.textContent = 'License suspended';
-    } else {
-      resultDiv.textContent = 'Points: ' + points;
-    }
-  }
   
-});
